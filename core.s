98b034743209c930abbcb4817bbe9c9eaad7075a
wilson dennison <wilson.dennison@gmail.com>
2022-10-24 18:20:23 +0200

    complete core.s

--- core.s
+++ core.s
@@ -10,19 +10,6 @@
 .global reset_handler
 
 /*
-* The actual vector table.
-* Only the size of RAM and 'reset' handler are
-* included for simplicity
-*/
-
-.type vtable, %object
-vtable:
-    .word _estack
-    .word reset_handler
-.size vtable, .-vtable
-
-
-/*
 * The reset handler. Called on reset
 */
 
@@ -33,14 +20,44 @@ reset_handler:
     LDR r0, =_estack
     MOV sp, r0
 
-    // Set some dummy values. When we see these values
-    // in our debugger we'll know that the program is loaded
-    // on the chip and working
-    LDR r7, =0xDEADBEEF
-    MOV r0, #0
-    main_loop:
-        // Add 1 to register r0
-        ADDS r0, r0, #1
-        // loop back
-        B   main_loop
+    // Copy data from flash to RAM data init section.
+    // R2 will store our progress along the sidata section.
+    MOVS r0, #0
+    // Load the start/end addresses of the data section,
+    // and the start of the data init section.
+    LDR  r1, =_sdata
+    LDR  r2, =_edata
+    LDR  r3, =_sidata
+    B    copy_sidata_loop
+    copy_sidata:
+      // Offset the data init section by our copy progress.
+      LDR  r4, [r3, r0]
+      // Copy the current word into data, and increment.
+      STR  r4, [r1, r0]
+      ADDS r0, r0, #4
+    copy_sidata_loop:
+      // Unless we've copied the whole data section, copy the
+      // next word from sidata->data.
+      ADDS r4, r0, r1
+      CMP  r4, r2
+      BCC  copy_sidata
+    // Once we are done copying the data section into RAM,
+    // move on to filling the BSS section with 0s.
+    MOVS r0, #0
+    LDR  r1, =_sbss
+    LDR  r2, =_ebss
+    B    reset_bss_loop
+    // Fill the BSS segment with '0's.
+    reset_bss:
+      // Store a 0 and increment by a word.
+      STR  r0, [r1]
+      ADDS r1, r1, #4
+    reset_bss_loop:
+      // We'll use R1 to count progress here; if we aren't
+      // done, reset the next word and increment.
+      CMP  r1, r2
+      BCC  reset_bss
+
+    // Branch to the 'main' method.
+    B    main
 .size reset_handler, .-reset_handler
